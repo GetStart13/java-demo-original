5c449f8a2493ccfc50583b30224227cf8348d682
fqq <2998657166@qq.com>
2024-05-20 09:11:45 +0800

    - 添加类实例描述

--- src/main/java/java类/ClassTypeWithMultiInterface.java
+++ src/main/java/java类/ClassTypeWithMultiInterface.java
@@ -21,11 +21,14 @@ public class ClassTypeWithMultiInterface implements Serializable, Comparable<Str
 
     public static void main(String[] args) {
         ClassTypeWithMultiInterface multiInterface = new ClassTypeWithMultiInterface();
-        if (multiInterface instanceof Serializable) {
-            System.out.println("实例属于 Serializable 类型");
-        }
-        if (multiInterface instanceof Comparable<?>) {
-            System.out.println("实例属于 Comparable 类型");
-        }
+        System.out.println("=============== 实例实现多个接口，则属于任何一接口的实例对象 ===============");
+        System.out.println("实例属于 Serializable 类型？" + (multiInterface instanceof Serializable));
+        System.out.println("实例属于 Comparable 类型？" + (multiInterface instanceof Comparable<?>));
+        System.out.println("=============== 类的数组不属于它的类实例，而是属于它的类数组实例 ===============");
+        System.out.println("类的数组对象是否属于它的类实例？" + ifClassArrayInstanceOfClass((new Integer[] {1, 2, 3})));
+    }
+
+    private static boolean ifClassArrayInstanceOfClass(Object obj) {
+        return obj instanceof Integer;
     }
 }
